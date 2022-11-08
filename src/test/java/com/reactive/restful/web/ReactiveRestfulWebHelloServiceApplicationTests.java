4df2c219b40f53f8644c34ca2bcfcb78ba361a6a
Choshi <Tokyo.Choshi@standardbank.co.za>
2022-11-08 15:42:24 +0200

    added project structure

---
+++ src/test/java/com/reactive/restful/web/ReactiveRestfulWebHelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.restful.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRestfulWebHelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
